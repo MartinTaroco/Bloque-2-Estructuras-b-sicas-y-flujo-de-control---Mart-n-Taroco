c024854143d4459c1845c7173a9b54314aa341e0
MartinTaroco <119903532+MartinTaroco@users.noreply.github.com>
2025-07-09 11:42:13 -0300

    Add files via upload

---
+++ 2_5_Desafio 1.py
@@ -0,0 +1,7 @@
+#Desafío 1: Saludo Personalizado
+#Escriba un programa que pida al usuario su nombre y su ciudad de origen, luego imprima un saludo personalizado con esa información.
+
+nombre = input("Dime tu nombre por favor: ")
+ciudad = input("Dime tu ciudad por favor: ")
+
+print(f"Hola {nombre} !! Tengo un conocido que tambien nacio en {ciudad}")
